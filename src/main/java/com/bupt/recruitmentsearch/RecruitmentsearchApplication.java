f8003cae1e858a812f4654d1d5e7864c0776e83c
StardustJK <1115159016@QQ.com>
2022-06-09 14:25:52 +0800

    hello world

---
+++ src/main/java/com/bupt/recruitmentsearch/RecruitmentsearchApplication.java
@@ -0,0 +1,13 @@
+package com.bupt.recruitmentsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmentsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bupt/recruitmentsearch/RecruitmentsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bupt.recruitmentsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
